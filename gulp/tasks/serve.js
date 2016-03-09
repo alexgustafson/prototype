d01830217b2e768d65d7b3a954f8318f25115a91
alexgustafson <alex_gustafson@yahoo.de>
2016-03-09 20:00:07 +0100

    django adaptations

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['build', 'watch'], function() {
+
+    browserSync({
+        online: false,            // online connectivity required?
+        host: '127.0.0.1',
+        port: 9000,               // different than django port
+        proxy: "127.0.0.1:8000",  // default django server url + port
+        startPath: "/prototype/", // open specific page
+        open: '127.0.0.1'         // start the browser
+    }, done);
+
+    gulp.watch("templates/**/*", ['']).on('change', browserSync.reload);
+    gulp.watch("static/**/*", ['']).on('change', browserSync.reload);
+})
